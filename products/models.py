4419d990812f40748eb4b23d6b891be64332edff
Stephen Scott <stephenjmscott@gmail.com>
2018-01-19 20:42:15 +0000

    removed unneeded rating

--- products/models.py
+++ products/models.py
@@ -40,7 +40,6 @@ class Review(models.Model):
     product = models.ForeignKey(Product, related_name="reviews")
     title = models.CharField(max_length=200)
     content = models.TextField()
-    rating = models.IntegerField()
     created_date = models.DateTimeField(auto_now_add=True)
     
     
